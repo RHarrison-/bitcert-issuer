3eedfd9434960a7043a200a5996d74d1d95f712c
Reece Harrison <reece@harrison.io>
2018-04-09 11:51:15 +0100

    batch certificate processing functional, able to create merkle proofs by reading in certificate json data. Added electrum functions to get tx data and broadcast tx through electrum network.

--- cert_handler.go
+++ cert_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -10,11 +11,15 @@ import (
 
 // Certificate  ... __
 type Certificate struct {
-	Type      string            `json:"type"`
-	Badge     badge             `json:"badge"`
-	IssuedOn  string            `json:"issuedOn"`
-	Recipient recipient         `json:"recipient"`
-	Signature *merkle.Signature `json:"signature"`
+	// structure of json output
+	Type      string            `json:"type,omitempty"`
+	Badge     badge             `json:"badge,omitempty"`
+	IssuedOn  string            `json:"issuedOn,omitempty"`
+	Recipient recipient         `json:"recipient,omitempty"`
+	Signature *merkle.Signature `json:"signature,omitempty"`
+
+	// internal processing values
+	valid bool
 }
 
 type recipient struct {
@@ -54,44 +59,67 @@ type badgeSignature struct {
 	Type     []string `json:"type"`
 }
 
-// func (c certificate) isSigned() bool {
-//     if c.Signature != nil {
-//         return true
-//     }
-//     return false
-// }
+// CalculateHash ... returns the hash of the certificates json string
+func (c Certificate) CalculateHash() []byte {
+	hashable, _ := json.Marshal(c)
+	h := sha256.New()
+	h.Write(hashable)
+	return h.Sum(nil)
+}
 
-type batch []Certificate
-
-// hash certs, craete merkle root, add proofs to certificates.
-func (b *batch) Sign() {
-	for _, cert := range *b {
-		fmt.Println(cert.Type)
+// check if signature filed is present and verify its content
+func (c Certificate) isSigned() bool {
+	if c.Signature != nil {
+		return true
 	}
-}
-
-func (b *batch) add(cert Certificate) {
-	*b = append(*b, cert)
-}
-
-func loadCertificate(path string) error {
-	var cert Certificate
-	data, _ := ioutil.ReadFile(path)
-
-	_ = json.Unmarshal(data, &cert)
-
-	// fmt.Println("cert.type", cert.Type)
-
-    var newBatch = batch{}
-    fmt.Println(len(newBatch))
-    newBatch.add(cert)
-
-	fmt.Println(len(newBatch), newBatch[0].Type)
-
-	return nil
+	return false
 }
 
 // make sure nessercary fields for signing are present
 func (c Certificate) validateIntegrity() {
 
+}
+
+// Batch ... Array of Certificate
+type Batch []*Certificate
+
+func (b Batch) toContentArray() []merkle.Content {
+	var content []merkle.Content
+	for _, c := range b {
+		content = append(content, c)
+	}
+
+	return content
+}
+
+func (b *Batch) test() {
+
+	tree, _ := merkle.NewTree(b.toContentArray())
+
+	tree.GenerateProofs()
+
+	var c Certificate
+	a, _ := json.Marshal(tree.Leafs[0].C)
+	_ = json.Unmarshal(a, c)
+	c.Signature = &tree.Leafs[0].Proof
+
+	fmt.Println(merkle.VerifyProof(*c.Signature))
+
+}
+
+func attachProof(n []merkle.Node) {
+	// for _, c := range b {
+	// 	content = append(content, c)
+	// }
+}
+
+func (b *Batch) add(cert *Certificate) {
+	*b = append(*b, cert)
+}
+
+func loadCertificate(path string) *Certificate {
+	var cert Certificate
+	data, _ := ioutil.ReadFile(path)
+	_ = json.Unmarshal(data, &cert)
+	return &cert
 }
